0ac50d179ea3f914326e64399f556b10a30b155e
surendra <surendramahawar873@gmail.com>
2022-09-10 20:28:20 +0530

    [Surendra].Created UI For Search Screen

--- GitHubFollowers/Controller/FavoritesListVC.swift
+++ GitHubFollowers/Controller/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GitHubFollowers
 //
 //  Created by Admin on 10/09/22.
@@ -7,11 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .systemBlue
     }
 }
-
