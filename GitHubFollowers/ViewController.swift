82a38610f46d40cc3fa791df07beccd23374c863
surendra <surendramahawar873@gmail.com>
2022-09-10 16:39:24 +0530

    intialized the project

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Admin on 10/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+This is my new project the name of this app is GitHub Follower in this 
+application user can enter the username of the github account and check 
+the follower of that user , they can also mark as a favorites user of the 
+github account.
